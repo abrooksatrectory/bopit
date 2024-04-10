5255f53887a6322c411018d02ce5ae2ba9dd2d38
abrooksatrectory <aidan.brooks@rectorywolves.org>
2024-04-10 13:57:20 -0400

    improved 3 functions, added game start/end

--- bop.py
+++ bop.py
@@ -1,18 +1,19 @@
-import random
+import random as rn
 import time
 from sense_hat import SenseHat
-sense = SenseHat
+
+sense = SenseHat()
 
 w = (255, 255, 255)
 b = (0, 0, 0)
 x = 3
 
-commands = ['up_arrow', 'push', 'twist']
+count = 0
+life = 1
 
-choice = random.choice(commands)
-
-
-up = [
+def up_arrow(x, count):
+	print("up!")
+	up = [
 b, b, b, w, w, b, b, b,
 b, b, w, w, w, w, b, b,
 b, w, w, w, w, w, w, b,
@@ -21,22 +22,20 @@ b, b, w, w, w, w, b, b,
 b, b, w, w, w, w, b, b,
 b, b, w, w, w, w, b, b,
 b, b, w, w, w, w, b, b]
-
-count = 0
-
-def up_arrow():
-	print("up!")
-	sense.set_pixels[arrow[0]]
+	
+	sense.set_pixels(up)
 	s=time.time()
-	up = sense.stick.wait_for_event()
+	up_var = sense.stick.wait_for_event()
 	e=time.time()
 	t=e-s
 	if push == "up" and t<x:
 		count = count + 1
+		x = x - 0.1
 	else:
+		life = 0
 		print('you lose')
 		
-def push():
+def push(x, count):
 	print('push it!')
 	s=time.time()
 	push = sense.stick.wait_for_event()
@@ -44,20 +43,36 @@ def push():
 	t = e-s
 	if push == "pressed" and t<x:
 		count = count + 1
+		x = x - 0.1
 	else:
+		life = 0
 		print('you lose')
 		
-def twist():
+def twist(x, count):
 	print('twist it!')
-	s = gyro.get_orientation('yaw')
-	wait(3)
-	e = gyro=sense.get_orientation('yaw')
+	sw = sense.get_orientation_degrees()
+	s = sw['yaw']
+	time.sleep(3)
+	ew = sense.get_orientation_degrees()
+	e = ew['yaw']
 	if -45 < s-e < 45:
+		life = 0
 		print('you lose')
+	else:
+		count = count + 1
+		x = x - 0.1
 
 
-	
-	
-	
+
+
+while(life == 1):
+	num = rn.randint(1,3)
+	if num == 1:
+		up_arrow(x, count)
+	elif num == 2:
+		push(x, count)
+	elif num == 3:
+		twist(x, count)
+print("game over. Final score: " + str(count))
 		
 
